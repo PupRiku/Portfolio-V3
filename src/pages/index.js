3b9ce86214304fa5e2b9c50c08949735f9c55e5b
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2024-05-02 20:54:05 +0000

    Add Lightbulb Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/components/Icons'
 import HireMe from '@/components/HireMe'
+import lightbulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
 
 export default function Home() {
   return (
@@ -56,6 +57,9 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe />
+        <div className='absolute right-8 bottom-8 inline-block w-24'>
+          <Image src={lightbulb} alt='Chris Diorio' className='w-full h-auto' />
+        </div>
       </main>
     </>
   )
