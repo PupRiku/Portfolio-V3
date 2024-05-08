196a3ac4ace25093057e20893a67ea251be2cacc
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2024-05-08 19:22:35 +0000

    Make fotter responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t-2 border-solid border-dark text-lg font-medium dark:border-light dark:text-light'>
-      <Layout className='flex items-center justify-between py-8'>
+    <footer className='w-full border-t-2 border-solid border-dark text-lg font-medium dark:border-light dark:text-light sm:text-base'>
+      <Layout className='flex items-center justify-between py-8 lg:flex-col lg:py-6'>
         <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
-        <div className='flex items-center'>
+        <div className='flex items-center lg:py-2'>
           Built With{' '}
           <span className='px-1 text-2xl text-primary dark:text-primaryDark'>
             &#9825;
