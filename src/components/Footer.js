288db10acf5d7ed8a0be9790763961d8e63aafb7
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2024-05-09 17:08:44 +0000

    Prettier Formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t-2 border-solid border-dark text-lg font-medium dark:border-light dark:text-light sm:text-base'>
+    <footer className='w-full border-t-2 border-solid border-dark text-lg font-medium sm:text-base dark:border-light dark:text-light'>
       <Layout className='flex items-center justify-between py-8 lg:flex-col lg:py-6'>
         <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
         <div className='flex items-center lg:py-2'>
